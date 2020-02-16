348791c3867112c991648b9212f60ca8dcb6b21c
zeta0134 <zeta0134@users.noreply.github.com>
2020-02-15 19:32:28 -0600

    Move the nds and cstdio headers here, add a HEADER helper for formatting

--- arm9/source/tests/testing_framework.h
+++ arm9/source/tests/testing_framework.h
@@ -1,12 +1,22 @@
 #ifndef TESTING_FRAMEWORK_H
 #define TESTING_FRAMEWORK_H
 
+#include <nds.h>
+#include <cstdio>
+
 #define TEST(test) do { \
     bool result = test(); \
     std::printf("%s %s\n", result ? "ok" : "not ok", #test); \
     nocashMessage((result ? "ok " : "not ok ")); \
     nocashMessage(#test); \
     nocashMessage("\n"); \
+  } while(false);
+
+#define HEADER(message) do { \
+  	std::printf("\n=== %s ===\n", message); \
+  	nocashMessage("\n=== "); \
+  	nocashMessage(message); \
+  	nocashMessage(" ===\n"); \
   } while(false);
 
 void init_test_console();
